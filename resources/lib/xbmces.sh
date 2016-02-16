eea8d8b4c9937ffc8a229446985815eae9824e45
romainhild <hild.romain@gmail.com>
2016-02-16 18:55:31 +0100

    use script command to launch emulationstation so that the emulators thinks they are in an interactive shell

--- resources/lib/xbmces.sh
+++ resources/lib/xbmces.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "stopping xbmc" > /home/xbian/test
 sudo service xbmc stop
-echo "starting emulationstation" >> /home/xbian/test
-emulationstation
-echo "starting xbmc" >> /home/xbian/test
+script -c "emulationstation" /dev/null
 sudo service xbmc start
-echo "end" >> /home/xbian/test
 
 
